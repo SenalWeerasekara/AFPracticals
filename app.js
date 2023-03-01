2efa2f6162d05fa873683433f547c50dfc7bf194
Semora <senalweerasekara@gmail.com>
2023-03-01 12:17:07 +0530

    adding read file

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 console.log("hello V8 is running here");
+console.log("hello V8 is running here");
+console.log("hello V8 is running here");
 // first stage the files
+
+// git add . 
+// git commit - m "message"
+// git push -u origin main
+
+// make new branch 
+
+// git branch feature/senal/console
+
+// now switch branches
+// git checkout feature/senal/console
+
+const fs = require('fs');
+fs.readFile('file.txt', 'utf8', function(err,data){
+        if(err)throw err;
+        console.log(data);
+})
