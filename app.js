5c43f26bc5fddaecd33a16b92cf065304f9f6474
Semora <senalweerasekara@gmail.com>
2023-03-01 12:01:34 +0530

    changes to new branch

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 console.log("hello V8 is running here");
+console.log("hello V8 is running here");
+console.log("hello V8 is running here");
 // first stage the files
+
+// git add . 
+// git commit - m "message"
+// git push -u origin main
+
+// make new branch 
+
+// git branch feature/senal/console
+
+// now switch branches
+// git checkout feature/senal/console
